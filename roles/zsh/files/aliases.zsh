88a3a107c43216da28679fea85971af5a2bf5540
Michael "Irish" O'Neill <irish.dot@gmail.com>
2021-01-10 12:50:39 -0500

    Add zsh role
    Add `zsh` role and include it in the `base-setup.yml` playbook.

---
+++ roles/zsh/files/aliases.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+# Some more ls aliases
+alias ls='ls --color=auto --group-directories-first --human-readable'
+alias ll='ls --human-readable -l'
+alias la='ls --almost-all'
+alias l='ls -CF'
+
+# Colorize grep output
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Colorize diff output
+alias diff='diff --color=auto'
